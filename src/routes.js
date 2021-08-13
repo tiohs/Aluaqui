3db9005ce953c9a6fb5b0e2c1f87e08b5c1e9168
tiohs <hamiltonsilva551@gmail.com>
2021-08-13 23:25:14 +0100

    Router

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './App/controllers/UserController';
+import EmovelController from './App/controllers/EmovelController';
 
 const router = new Router();
 
 router.get('/user', UserController.index);
 router.post('/user', UserController.store);
+router.get('/imovel', EmovelController.store);
+router.post('/imovel', EmovelController.index);
 
 export default router;
