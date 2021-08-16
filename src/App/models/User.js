f7c3cf29a7e5fa8f222ba2d11f6c82dec94fa28b
tiohs <hamiltonsilva551@gmail.com>
2021-08-16 16:05:39 +0100

    model User

---
+++ src/App/models/User.js
@@ -0,0 +1,32 @@
+import { Model, DataTypes } from 'sequelize';
+import { hash, compare } from 'bcryptjs';
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.VIRTUAL,
+        password_hash: DataTypes.STRING,
+        number: DataTypes.STRING,
+        provider: DataTypes.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await hash(user.password, 8);
+        user.provider = false;
+      }
+    });
+    return this;
+  }
+ 
+  checkPassword(password) {
+    return compare(password, this.password_hash);
+  }
+}
+
+export default User;
