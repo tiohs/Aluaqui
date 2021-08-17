9889f0b1284dad7175fdc39ced2e8d30904bc151
tiohs <hamiltonsilva551@gmail.com>
2021-08-18 00:26:31 +0100

    config multer

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 // Middlewares
 import authMiddleware from './App/middlewares/auth';
 // Controllers 
+import multerConfig from './config/multer';
 import UserController from './App/controllers/UserController';
 import ImovelController from './App/controllers/ImovelController';
 import SessionController from './App/controllers/SessionControllers';
@@ -12,6 +13,7 @@ const router = new Router();
 router.post('/sessions', SessionController.store);
 router.post('/user', UserController.store);
 router.use(authMiddleware);
+router.post('/file', upload.single('file'), FileController.store);
 router.post('/imovel', ImovelController.store);
 router.get('/imovel', ImovelController.index);
 
