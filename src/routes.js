40d193a8a20a60cceaaf20d8e96f4465a755a3bf
tiohs <hamiltonsilva551@gmail.com>
2021-08-18 00:28:25 +0100

    Files

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 // Middlewares
 import authMiddleware from './App/middlewares/auth';
@@ -7,8 +8,10 @@ import multerConfig from './config/multer';
 import UserController from './App/controllers/UserController';
 import ImovelController from './App/controllers/ImovelController';
 import SessionController from './App/controllers/SessionControllers';
+import FileController from './App/controllers/SessionControllers';
 
 const router = new Router();
+const upload = multer(multerConfig);
 
 router.post('/sessions', SessionController.store);
 router.post('/user', UserController.store);
